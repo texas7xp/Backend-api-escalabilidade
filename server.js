77d2b7e060eae7b1689b02e8bb49b2f2bf557d1d
texas7xp <159555851+texas7xp@users.noreply.github.com>
2025-10-01 13:46:40 +0000

    commit 1

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.get("/", (req, res) => {
+  res.json({
+    mensagem: "API funcionando no Render ■",
+    hora: new Date().toISOString(),
+  });
+});
+app.get("/alunos", (req, res) => {
+  res.json([
+    { nome: "Ana", nota: 8 },
+    { nome: "Bruno", nota: 7 },
+  ]);
+});
