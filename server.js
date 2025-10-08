6074420b79f00bc3054b8e70443c80c7317ebc2c
texas7xp <159555851+texas7xp@users.noreply.github.com>
2025-10-08 09:27:06 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,17 +2,14 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3000;
-
 app.use(cors());
 app.get("/", (req, res) => {
-  res.json({
-    mensagem: "API funcionando no Render ■",
-    hora: new Date().toISOString(),
-  });
+res.json({ mensagem: "API funcionando no Render ■", hora: new Date().toISOString() });
 });
 app.get("/alunos", (req, res) => {
-  res.json([
-    { nome: "Ana", nota: 8 },
-    { nome: "Bruno", nota: 7 },
-  ]);
+res.json([
+{ nome: "Ana", nota: 8 },
+{ nome: "Bruno", nota: 7 }
+]);
 });
+app.listen(PORT, () => console.log(`Servidor ouvindo na porta ${PORT}`));
